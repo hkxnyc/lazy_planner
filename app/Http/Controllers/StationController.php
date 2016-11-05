dafc6956487afd9a7ec1168dc693a9dab8ae34c6
Hannah Kim <hrk624@Hannahs-MBP-2.fios-router.home>
2016-11-05 10:39:09 -0400

    added search query box

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -10,13 +10,17 @@ use App\Http\Requests;
 
 class StationController extends Controller
 {
-    public function getYelpData($stations)
+    public function getYelpData($stations,$query = null)
     {
         $yelp = new YelpController();
         $stations = Station::find($stations);
         $businesses = [];
         foreach ($stations as $station) {
-            $yelpData = $yelp->getData($station->positionArray());
+            $queryInput = $station->positionArray();
+            if($query && trim($query) !== ''){
+                $queryInput['term'] = trim($query);
+            }
+            $yelpData = $yelp->getData($queryInput);
             $businesses = array_merge($businesses, array_map(function ($obj) use ($station) {
                 $obj->station = $station->id;
                 return $obj;
@@ -30,7 +34,14 @@ class StationController extends Controller
 
     public function showYelpData(Request $request)
     {
-        $data = $this->getYelpData($request->get('stations'));
+        $this->validate($request,[
+           'stations' => 'required|array|min:3|max:5'
+        ],[
+            'stations.required' => 'You must pick 3 stations to search near.',
+            'stations.min' => 'You must pick at least 3 stations to search near and no more than 5',
+            'stations.max' => 'You must pick no more that 5 stations to search near and no less than 3'
+        ]);
+        $data = $this->getYelpData($request->get('stations'),$request->get('query'));
         $s = Search::createNewSearch($data);
         return redirect()->route('stations.search',['slug'=>$s->slug]);
     }
